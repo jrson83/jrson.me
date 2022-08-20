f59964cda7bc60d2cb1977497c602c5c94002a4c
jrson83 <me@jrson.de>
2022-08-20 04:24:48 +0200

    updated deps

--- plugins/preactjsx/deps.ts
+++ plugins/preactjsx/deps.ts
@@ -1,2 +1,2 @@
-export * from "https://esm.sh/preact@10.10.1";
-export { renderToString } from "https://esm.sh/preact-render-to-string@5.2.1?deps=preact@10.10.1";
+export * from "https://esm.sh/preact@10.10.6";
+export { renderToString } from "https://esm.sh/preact-render-to-string@5.2.2?deps=preact@10.10.6";
