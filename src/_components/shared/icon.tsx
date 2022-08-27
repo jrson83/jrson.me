d02209b590efe8862e7a9d77c856579645a4bb24
jrson83 <me@jrson.de>
2022-08-28 01:09:36 +0200

    removed svg inline style

--- src/_components/shared/icon.tsx
+++ src/_components/shared/icon.tsx
@@ -7,36 +7,22 @@ type IconProps = {
 };
 
 export default ({ className, color, icon, size = 32, title }: IconProps) => {
-  const styles = {
-    svg: {
-      display: "inline-block",
-      verticalAlign: "middle",
-    },
-    path: {
-      fill: color
-        ? color.startsWith("#") ? color : `var(--${color})`
-        : "currentColor",
-      fillRule: "evenodd",
-    },
-  };
-
   return (
     <svg
       aria-hidden="true"
       role="img"
+      fill={color
+        ? color.startsWith("#") ? color : `var(--${color})`
+        : "currentColor"}
+      fill-rule="evenodd"
       className={className}
-      /* style={styles.svg} */
       width={`${size}px`}
       height={`${size}px`}
       preserveAspectRatio="xMidYMid meet"
       viewBox="0 0 24 24"
     >
       <title>{title}</title>
-      <path
-        style={styles.path}
-        d={icon}
-      >
-      </path>
+      <path d={icon}></path>
     </svg>
   );
 };
