b225defcfd24041ce0d6cc7b6a05a6b03eb0f59c
jrson83 <me@jrson.de>
2022-08-28 16:33:04 +0200

    optimized badge [skip actions]

--- src/_components/blog/tag.tsx
+++ src/_components/blog/tag.tsx
@@ -5,11 +5,11 @@ export default ({ tags }: PageData, { urlFilter }: PageHelpers) => {
     <>
       {tags?.map((tag: string, index: number) => {
         return (
-          <div key={index} className="bdg bdg-tag" itemProp="keywords">
+          <div key={index} className="bdg bdg--tag" itemProp="keywords">
             #{tag}
             <a href={urlFilter!(`/blog/tag/${tag}/`)} rel="tag">
               <span
-                className="bdg-link"
+                className="bdg--link"
                 role="link"
                 tabIndex={0}
                 aria-label={tag}
