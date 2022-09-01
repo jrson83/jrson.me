b847f324ac4f558a83c23d031e12f9b1a18f9895
jrson83 <me@jrson.de>
2022-09-02 01:23:56 +0200

    removed x-ua-compatible [no actions]

--- src/_includes/layouts/root.tsx
+++ src/_includes/layouts/root.tsx
@@ -17,7 +17,6 @@ export default (
       <script type="module" nonce="CSP_NONCE" src={urlFilter!(`/scripts/main.js`)} inline />
       <head>
         <meta charSet="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{`${title} - ${site.title}`}</title>
 
